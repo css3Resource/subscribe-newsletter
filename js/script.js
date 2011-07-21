1a20390f2590e3f999780e0b4895d3986b4c7bd9
Francesco Fonte <webffranz@gmail.com>
2011-07-22 01:23:34 +0200

    subscribe-newsletter

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-/*  Your JavaScript  */
+/*  Your JavaScript for Resource  */
+
+$(document).ready(function(){
+
+	Utils.init();
+
+});
+
+var Utils  = {
+	
+	init : function(){
+	
+		$('.menutop').append(' &mdash; <a href="http://www.premiumpixels.com/freebies/newsletter-sign-up-form-psd/" target="_blank">Inspired by: Pixel Premium</a> ')
+	
+	}
+	
+}
